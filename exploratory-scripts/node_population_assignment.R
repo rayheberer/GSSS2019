adda59aaa7766259d4dfcaa8840c98bd2a5b2768
Raymond Heberer <ray.heberer@gmail.com>
2019-07-23 22:46:54 +0000

    Join income data to census_data_sf

--- exploratory-scripts/node_population_assignment.R
+++ exploratory-scripts/node_population_assignment.R
@@ -7,8 +7,8 @@ tmpdir <- tempdir()
 totalcensus::set_path_to_census(tmpdir)
 
 city <- "santa fe, new mexico"
-state_abbr <- "NM"
-county_fips <- 049
+state <- "NM"
+county <- 049
 osm_tags <- c("motorway", "trunk", "primary", "secondary", "tertiary")
 
 # Import/Process Graph Data -----------------------------------------------
@@ -26,22 +26,38 @@ distances <- dodgr::dodgr_distances(graph) %>%
 
 # Import Census Data ------------------------------------------------------
 
-population <- totalcensus::read_decennial(
+population <- 
+  totalcensus::read_decennial(
     year = 2010, 
-    states = state_abbr,
-    summary_level = "block group"
+    states = state, 
+    summary_level = "block group", 
   ) %>% 
   dplyr::mutate(GEOID = stringr::str_sub(GEOID, 8L)) %>% 
   dplyr::select(GEOID, population)
 
-block_groups_sf <- tigris::block_groups(state_abbr, county_fips) %>% 
+income <- tidycensus::get_acs("tract", "B19013_001", state = state, county = county) %>% 
+  dplyr::select(GEOID, income = estimate, income_moe = moe)
+
+
+# Join Census Data to Shapefiles ------------------------------------------
+
+tracts_sf <- tigris::tracts(state, county) %>% 
   sf::st_as_sf()
 
-population_sf <- block_groups_sf %>% 
-  dplyr::left_join(population) %>% 
-  dplyr::select(GEOID, population) %>% 
+block_groups_sf <- tigris::block_groups(state, county) %>% 
+  sf::st_as_sf()
+
+income_tracts <- tracts_sf %>% 
+  dplyr::left_join(income, by = "GEOID") %>% 
+  tibble::as_tibble() %>% 
+  dplyr::select(-geometry, -GEOID)
+
+census_data_sf <- block_groups_sf %>% 
+  dplyr::left_join(population, by = "GEOID") %>% 
+  dplyr::left_join(income_tracts, by = "TRACTCE") %>% 
+  dplyr::select(GEOID, population, income, income_moe) %>% 
   dplyr::mutate(area = sf::st_area(geometry), density = population / area)
 
 # Distribute Population to Nodes ------------------------------------------
 
-nodes <- get_nodes_with_populations(graph, population_sf, distances)
+nodes <- get_nodes_with_census_data(graph, census_data_sf, distances)
