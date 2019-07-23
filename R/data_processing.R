a40d0f4a7d29aa0aa0053227631c80a2533795b4
Raymond Heberer <ray.heberer@gmail.com>
2019-07-23 21:47:14 +0000

    Extend disconnected node detection to recursively trim network

--- R/data_processing.R
+++ R/data_processing.R
@@ -1,6 +1,16 @@
-get_disconnected_nodes <- function(distances) {
-  sum_missing <- rowSums(is.na(distances))
+trim_disconnected_nodes <- function(distances) {
+  trimmed <- FALSE
   
-  sum_missing[sum_missing == length(sum_missing) - 1] %>% 
-    names()
+  while (!trimmed) {
+    sum_missing <- rowSums(is.na(distances))
+    trim <- names(sum_missing[sum_missing == length(sum_missing) - 1])
+    
+    if (length(trim) > 0) {
+      distances <- distances[!rownames(distances) %in% trim, !colnames(distances) %in% trim]
+    } else {
+      trimmed <- TRUE
+    }
+  }
+  
+  distances
 }
