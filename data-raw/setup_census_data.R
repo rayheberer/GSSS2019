a02ed739b0ac88a55c06257972d8fdcfb1c81086
Raymond Heberer <ray.heberer@gmail.com>
2019-07-25 23:42:56 +0000

    Unlink tempdir at end of script

--- data-raw/setup_census_data.R
+++ data-raw/setup_census_data.R
@@ -4,11 +4,14 @@ library(totalcensus)
 source('~/sfi_project/R/data_processing.R')
 totalcensus::set_path_to_census(tempdir())
 
-state <- "NM"
-county <- "049"
+state <- "MA"
+county <- "025"
 
 # -------------------------------------------------------------------------
 
 census_data <- import_join_population_income_data(state, county)
 
 saveRDS(census_data, file.path("data", paste0(state, county, "_censusdata.rds")))
+
+unlink(tempdir(), TRUE)
+gc()
