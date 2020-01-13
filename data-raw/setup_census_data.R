7a1b93721e36220136aa1483194ee278ef96bec4
Raymond Heberer <ray.heberer@gmail.com>
2020-01-12 17:01:27 -0700

    Cache raw files locally

--- data-raw/setup_census_data.R
+++ data-raw/setup_census_data.R
@@ -2,16 +2,22 @@ library(magrittr)
 library(totalcensus)
 
 source(here::here('R/data_processing.R'))
-totalcensus::set_path_to_census(tempdir())
 
-state <- "MA"
-county <- "025"
+options(tigris_use_cache = TRUE)
+
+data_path <- here::here("data/totalcensus")
+
+totalcensus::set_path_to_census(data_path)
+tigris::tigris_cache_dir(here::here("data/tigris_cache"))
+dir.create(data_path, recursive = TRUE, showWarnings = FALSE)
+
+state <- "GA"
+county_fips <- "121"
 
 # -------------------------------------------------------------------------
 
-census_data <- import_join_population_income_data(state, county)
+census_data <- import_join_population_income_data(state, county_fips)
 
-saveRDS(census_data, file.path("data", paste0(state, county, "_censusdata.rds")))
+saveRDS(census_data, file.path(here::here("data"), paste0(state, county_fips, "_censusdata.rds")))
 
-unlink(tempdir(), TRUE)
 gc()
